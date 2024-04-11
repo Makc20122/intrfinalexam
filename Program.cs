9b7fda3b348307760d53aea389b0f34e6d481a10
Maksim Kozyrev <kozyrev.maxime@gmail.com>
2024-04-11 20:41:06 +0300

    task accomplished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Start");
+int j = 0;
+string[] Array = {"Une", "orange", "sur", "la", "table", "Ta", "robe", "sur", "le", "tapis", "Et", "toi", "dans", "mon", "lit"};
+string[] Array2 = new string[Array.Length];
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+    {
+        Array2[j] = Array[i];
+        j += 1;
+    }
+}
+
+printArray(Array2);
+
+void printArray(string[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
